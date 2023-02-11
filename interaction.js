5db3d7f1daf37cfc67daea8b0f349a8a4e87e036
lise gomes <lise.gomes56@gmail.com>
2023-02-11 15:02:57 +0100

    Implementation of interactions.js + start of model.js

--- interaction.js
+++ interaction.js
@@ -3,10 +3,52 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+	var dragX = 0;
+	var dragY = 0;
+	var dropX = 0;
+	var dropY = 0;
+	var isMousePressed = false;
 
 	// Developper les 3 fonctions gérant les événements
+	DnD.prototype.mousePressed = function(event) {
+        isMousePressed = true;
+        dragX = getMousePosition(canvas, event).x;
+        dragY = getMousePosition(canvas, event).y;
+        console.log("Initial position : " + dragX + " - " + dragY);
+	};
+
+	DnD.prototype.mouseMoved = function(event) {
+	    if(isMousePressed) {
+            dropX = getMousePosition(canvas, event).x;
+            drop_y = getMousePosition(canvas, event).y;
+            console.log("Current position : " + dropX + " - " + dropY);
+        }
+    };
+
+    DnD.prototype.mouseReleased = function(event) {
+        if(isMousePressed) {
+            dropX = getMousePosition(canvas, event).x;
+            dropY = getMousePosition(canvas, event).y;
+            console.log("Final position : " + dropX + " - " + dropY);
+
+            // for testing
+            var ctx = canvas.getContext('2d');
+            //Draws the path
+            ctx.beginPath();
+            ctx.moveTo(dragX, dragY);
+            ctx.lineTo(dropX, dropY);
+            ctx.closePath();
+
+            ctx.stroke();
+
+            isMousePressed = false;
+        }
+    };
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+	canvas.addEventListener('mousedown', this.mousePressed, false);
+	canvas.addEventListener('mousemove', this.mouseMoved, false);
+	canvas.addEventListener('mouseup', this.mouseReleased, false);
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,23 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+/**
+Une forme possède une couleur et une épaisseur de trait. Un rectangle possède des coordonnées de
+son point haut-gauche, une largeur et une hauteur. Une ligne possède les coordonnées de ses deux points.
+**/
+
+function Drawing() {
+    var shapes[];
+}
+
+function Shape(color, lineWidth) {
+
+    function Line(color, lineWidth, startX, startY, endX, endY) {
+      //Shape.call(this, color, lineWidth);
+    };
+
+    function Rectangle(color, lineWidth) {
+      //Shape.call(this, color, lineWidth);
+    };
+
+}
